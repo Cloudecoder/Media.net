c7e5f703790da1f9170a257e6d91d6f2d03d5128
Cloudecoder <70545534+Cloudecoder@users.noreply.github.com>
2022-05-23 10:19:48 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,68 +1,33 @@
-# importing libraries
+
 from bs4 import BeautifulSoup
+import pandas as pd
 import requests
+page_count=int(input("enter pages"))
 
-def main(URL):
-	# opening our output file in append mode
-	File = open("out.csv", "a")
-
-HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
-
-
-	# Making the HTTP Request
-	webpage = requests.get(URL, headers=HEADERS)
-
-	# Creating the Soup Object containing all data
-	soup = BeautifulSoup(webpage.content, "lxml")
-
-	# retrieving product title
-	try:
-		# Outer Tag Object
-		title = soup.find("span",
-						attrs={"id": 'productTitle'})
-
-		# Inner NavigableString Object
-		title_value = title.string
-
-		# Title as a string value
-		title_string = title_value.strip().replace(',', '')
-
-	except AttributeError:
-		title_string = "NA"
-	print("product Title = ", title_string)
-
-	# saving the title in the file
-	File.write(f"{title_string},")
-
-	# retrieving price
-	try:
-		price = soup.find(
-			"span", attrs={'id': 'priceblock_ourprice'})
-								.string.strip().replace(',', '')
-		# we are omitting unnecessary spaces
-		# and commas form our string
-	except AttributeError:
-		price = "NA"
-	print("Products price = ", price)
-
-	# saving
-	File.write(f"{price},")
-
-	try:
-		review_count = soup.find(
-			"span", attrs={'id': 'acrCustomerReviewText'})
-								.string.strip().replace(',', '')
-
-	except AttributeError:
-		review_count = "NA"
-	print("Total reviews = ", review_count)
-	File.write(f"{review_count},")
+ph_name=[]
+ph_price=[]
+ph_rating=[]
 
 
-if __name__ == '__main__':
-# opening our url file to access URLs
-	file = open("url.txt", "r")
+for i in range(1,page_count+1):
+    url="https://www.amazon.in/s?k=mobile&i=electronics&crid=1QKI8R9TVMUQG&sprefix=mobile%2Celectronics%2C1406&ref=nb_sb_noss_"+str(i)
+    req=requests.get(url)
+    content=BeautifulSoup(req.content, 'html.parser')
+    name=content.find_all('span',{"class":"a-size-medium a-color-base a-text-normal"})
+    price=content.find_all('span',{"class":"a-price-whole"})
+    rating=content.find_all('span',{"class":"a-size-base s-light-weight-text"})
 
-	# iterating over the urls
-	for links in file.readlines():
-		main(links)
+    for i in name:
+        print(i.text)
+        ph_name.append(i.text)
+    for i in price:
+        print(i.text)
+        ph_price.append(i.text)
+    for i in rating:
+        print(i.text)
+        ph_rating.append(i.text)
+data={"phone name":ph_name,"phone price":ph_price,"phone rating":ph_rating}
+df=pd.DataFrame(data)
+print(df)  
+
+   
